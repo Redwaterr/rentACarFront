34629513d8422e172430fcb16f5cb37b16081cae
Ugur <ugur@localhost.localdomain>
2018-12-23 14:27:03 +0300

    last2

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/frontEnd'));
+app.use(express.static(__dirname + '/dist/front-end'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/frontEnd/index.html'));
+res.sendFile(path.join(__dirname+'/dist/front-end/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
